17e0e9fec207c1b683c7c8f9451d0cb7c93e8f39
OmitHasanAdor <ibneshams05@gmail.com>
2026-06-17 00:28:42 +0600

    add google verification file

---
+++ src/app/robots.js
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+
+    sitemap:
+      "https://porto-dot-omit-hasan.vercel.app/sitemap.xml",
+  };
+}
---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://porto-dot-omit-hasan.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
